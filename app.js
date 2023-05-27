e4fa91132eb35710d7f559878d559596aa3932a3
Christian Kallas <bkallas3@gmail.com>
2023-05-27 12:19:12 -0700

    refactored HTML

---
+++ app.js
@@ -0,0 +1,69 @@
+console.log(
+  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+);
+
+// Exercise 1
+console.log("EXERCISE 1:\n==========\n");
+
+// YOUR CODE HERE
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+console.log(add);
+
+// Exercise 2
+console.log("EXERCISE 2:\n==========\n");
+
+// YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+console.log(num + str); 
+// "1111"
+console.log(num + str2);
+// 11eleven
+console.log(num + isPresent);
+// 12
+console.log(firstName + num);
+// "Frodo11"
+console.log(isPresent + str);
+// "truell"
+console.log(firstName + lastName);
+// "FrodoBaggins"
+
+
+
+// Exercise 3
+console.log("EXERCISE 3:\n==========\n");
+
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+
+// YOUR CODE HERE
+console.log(val == str3);
+// true
+console.log(val === str3);
+// false
+console.log(!isAwake);
+// true
+console.log("eleven" == str4 && val >= str3);
+// false
+console.log(!isAwake || isAwake);
+// true
+console.log(0 == false);
+// true
+console.log(0 === false);
+// false
+console.log(0 != false);
+// false
+console.log(0 !== false);
+// true
